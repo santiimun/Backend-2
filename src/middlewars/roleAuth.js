643be1dc4aaa09b67ee6897548fd56eaf94b9d1a
santi <santiago.ezequiel.munoz@gmail.com>
2025-03-27 19:07:54 -0300

    proyecto final backend 2

--- src/middlewars/roleAuth.js
+++ src/middlewars/roleAuth.js
@@ -1,6 +1,6 @@
 export const roleAuth = (...roles) => {
     return async(req, res, next) => {        
-        if (!roles.includes(req.user.role)) {
+        if (!roles.includes(req.user.role.toLowerCase())) {
             return res.status(403).json({ error: 'Unauthorized' });
         }
         next();
